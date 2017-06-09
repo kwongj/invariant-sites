4985e6bb0040fbda5fa03709a2af37dc55100a59
Jason Kwong <kwongj@gmail.com>
2017-06-10 03:13:12 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # invariant-sites
 Counts invariant/variant and masks variant sites from multi-FASTA alignment
+
+## Author
+
+Jason Kwong (@kwongjc)
+
+## Dependencies
+* Python 2.7.x
+* BioPython
+
+## Usage
+
+```
+$ invariant-sites.py -h
+usage: 
+  invariant-sites.py [--out invariant.fa] FASTA
+
+Counts invariant/variant and masks variant sites from multi-FASTA alignment
+
+positional arguments:
+  FASTA       original multi-FASTA alignment file
+
+optional arguments:
+  -h, --help  show this help message and exit
+  --out FILE  specify output file with variant sites masked (default = stdout)
+  --mask N    symbol to use for masking; change this if the alignment already contains this symbol
+  --version   show program's version number and exit
+```
+
+## Bugs
+
+Please submit via the [GitHub issues page](https://github.com/kwongj/invariant-sites/issues).  
+
+## Software Licence
+
+[GPLv3](https://github.com/kwongj/invariant-sites/blob/master/LICENSE)
+
---
+++ invariant-sites.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+# Usage
+import argparse
+from argparse import RawTextHelpFormatter
+import os
+import sys
+import re
+from Bio import Seq
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+import pandas as pd
+from pandas import DataFrame
+from collections import Counter
+
+# Functions
+# Log a message to stderr
+def msg(*args, **kwargs):
+	print(*args, file=sys.stderr, **kwargs)
+
+# Log an error to stderr and quit with non-zero error code
+def err(*args, **kwargs):
+	msg(*args, **kwargs)
+	sys.exit(1);
+
+# Check file exists
+def check_file(f):
+	return os.path.isfile(f)
+
+# Check if file is in FASTA format
+def check_fasta(f):
+	if not os.path.isfile(f) or os.path.getsize(f) < 1:
+		return False
+	with open(f, 'r') as fasta:
+		if fasta.readline()[0] != '>':						# Check if header starts with ">"
+			return False
+		for line in fasta:
+			line = line.strip()
+			if not line or line[0] == '>':	
+				continue
+			if bool(re.search('[^ACTGactgNn\-]', line)):	# Check if there are non-nucleotide characters in sequence
+				return False
+	return True
+
+parser = argparse.ArgumentParser(
+	formatter_class=RawTextHelpFormatter,
+	description='Counts invariant/variant and masks variant sites from multi-FASTA alignment\n',
+	usage='\n  %(prog)s [--out invariant.fa] FASTA')
+parser.add_argument('fasta', metavar='FASTA', nargs=1, help='original multi-FASTA alignment file')
+parser.add_argument('--out', metavar='FILE', nargs=1, help='specify output file with variant sites masked (default = stdout)')
+parser.add_argument('--mask', metavar='N', default='N', nargs=1, help='symbol to use for masking; change this if the alignment already contains this symbol')
+parser.add_argument('--version', action='version', version='%(prog)s v0.1')
+args = parser.parse_args()
+
+# Check input/output files
+if not check_file(args.fasta[0]):
+	err('ERROR: Cannot find "{}". Check file exists in the specified directory.'.format(args.fasta[0]))
+if not check_fasta(args.fasta[0]):
+	err('ERROR: Check "{}" is in FASTA format.'.format(args.fasta[0]))
+if args.out:
+	if check_file(args.out[0]):
+		err('ERROR: "{}" already exists.'.format(args.out[0]))
+
+# Parse sequences as lists
+seqlist = []
+for record in SeqIO.parse(args.fasta[0], 'fasta'):
+	msg('Reading {} ...'.format(record.id))
+	seqlist.append(list(record.seq.upper()))
+
+# Import into pandas dataframe
+msg('Converting to dataframe and counting sites ... please wait')
+df = pd.DataFrame(seqlist)
+
+#print(df)
+# Mask variant sites as '-'
+for col in df:
+	if len(set(df[col])) > 1:
+		df[col] = args.mask[0]
+
+# Export 1st row as consensus (given they are all the same after masking variants)
+newseq = df.iloc[0].tolist()
+
+# Count nucleotides and variant sites in sequence
+for site in Counter(newseq):
+	print(''.join([site, ': ', str(Counter(newseq)[site])]))
+
+# Export masked sequences to FASTA output
+newseqs = []
+for record in SeqIO.parse(args.fasta[0], 'fasta'):
+	newseqs.append(SeqRecord(Seq.Seq(''.join(newseq)), id=record.id, description=record.description))
+
+if args.out:
+	msg('Masked sequences saved to "{}" ... '.format(args.out[0]))
+	SeqIO.write(newseqs, args.out[0], 'fasta')
+
+sys.exit(0)
+
